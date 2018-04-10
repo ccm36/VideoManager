d84c9a65d026f511c8f276cf754cd64ff3148cfa
ccm36 <cameron.c.malloy@gmail.com>
2018-04-10 10:30:12 -0500

    Add show titles to search page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import preload from '../../data.json';
 
-const Search = () => <div>This is my future search page!</div>;
+const Search = () => <div className="search">{preload.shows.map(show => <h3>{show.title}</h3>)}</div>;
 
 export default Search;
