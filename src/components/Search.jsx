650307a8dcb1b4c551b2da298ba3830483fb7c1f
ccm36 <cameron.c.malloy@gmail.com>
2018-04-12 20:31:37 -0500

    Turn ShowCard into styled component and add styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ShowCard from './ShowCard';
 import preload from '../../data.json';
 
-const Search = () => <div className="search">{preload.shows.map(show => <ShowCard show={show} />)}</div>;
+const Search = () => (
+  <div className="search">
+    <div>{preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}</div>
+  </div>
+);
 
 export default Search;
