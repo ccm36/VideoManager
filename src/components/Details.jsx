e3d66b1f590c5838dc79935d198b1bbe9a29c2d4
ccm36 <cameron.c.malloy@gmail.com>
2018-04-17 10:45:08 -0500

    Implement Details page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,10 +2,30 @@
 
 import React from 'react';
 
-const Details = () => (
-  <div className="details">
-    <h1>hi</h1>
-  </div>
-);
+const Details = (props: { show: Show }) => {
+  const { title, description, year, poster, trailer } = props.show;
+
+  return (
+    <div className="details">
+      <header>
+        <h1>Video Manager</h1>
+      </header>
+      <section>
+        <h1>{title}</h1>
+        <h2>({year})</h2>
+        <img alt={`Poster for ${title}`} src={`/public/img/posters/${poster}`} />
+        <p>{description}</p>
+      </section>
+      <div>
+        <iframe
+          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+          frameBorder="0"
+          allowFullScreen
+          title={`Trailer for ${title}`}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Details;
